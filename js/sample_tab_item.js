96ab68932d3b69aef4bf15624672945972764ddf
Juha-Jarmo Heinonen <o@rsence.org>
2010-05-30 19:44:32 +0300

    Improved button and sample tab item code. Not complete, will continue with the other components; targeting release version.

--- js/sample_tab_item.js
+++ js/sample_tab_item.js
@@ -5,12 +5,16 @@ SampleTabItem = HClass.extend({
     var _tabItem = HTabItem.nu(_rect,_tab,_options);
     
     /* Description: */
-    HStringView.nu(
+    this.descriptionView = HStringView.nu(
       [ 8, 8, null, _options.descr_height, 8, null ],
       _tabItem, {
         value: '<h1>'+_options.label+'</h1>'+_options.descr
       }
     );
+    
+    ELEM.flushLoop();
+    var _descrHeight = ELEM.getSize(this.descriptionView.markupElemIds.value)[1];
+    console.log('descrHeight',_descrHeight);
     
     var _sampleTop = _options.descr_height+42,
         _sampleHeight = (_options.sample_height > _options.yaml_height)?_options.sample_height:_options.yaml_height;
@@ -39,20 +43,27 @@ SampleTabItem = HClass.extend({
       }
     );
     
+    var _patternUrl = _tab.app.options.checker_pattern_gif_url;
+    
     /* Sample Viewer area */
     COMM.Queue.push(
-      function(_json,_class,_sampleTop,_sampleHeight,_tabItem){
+      function(_json,_style,_sampleTop,_sampleHeight,_tabItem){
         // debugger;
         JSONRenderer.nu(
           _json,
-          _class.nu(
+          HView.nu(
             [ 8, _sampleTop, null, _sampleHeight, 416, null ],
-            _tabItem, {}
+            _tabItem, {
+              style: _style
+            }
           )
         );
       },
       [_options.json,
-      SampleView,
+      [ ['border','1px solid #000'],
+        ['background-color','#e6e7e9'],
+        ['background-image','url('+_patternUrl+')']
+      ],
       _sampleTop,
       _sampleHeight,
       _tabItem]
